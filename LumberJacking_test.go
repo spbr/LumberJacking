5aff64778be6c65514ae7ec10f1a6d4f05fda542
Shane P. Brady <shane.p.brady@gmail.com>
2016-11-30 22:51:39 -0600

    Added more test coverage

--- LumberJacking_test.go
+++ LumberJacking_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"testing"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 	"io/ioutil"
-	"encoding/json"
+	"os"
+	"testing"
+	"sync"
 )
 
-var tgConf = LogConfig{
-}
+var tgConf = LogConfig{}
 var tgLoggers map[string]*Logger
 
 var tconfigFile string
@@ -70,4 +70,63 @@ func TestLog(t *testing.T) {
 	if err != nil {
 		t.Error("Didn't expect error ", err)
 	}
+}
+
+func TestPerf(t *testing.T) {
+	flag.Usage = func() {
+		fmt.Println("Must supply a configuration file")
+		os.Exit(64)
+	}
+
+	flag.Parse()
+
+	if tconfigFile == "" {
+		flag.Usage()
+	}
+	file, err := ioutil.ReadFile(tconfigFile)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(file, &tserverConfig)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	tgLoggers = make(map[string]*Logger, tserverConfig.MaxLogs)
+
+	err = Init(tserverConfig.LogHome, tserverConfig.MaxMinutes)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	err = os.MkdirAll(tserverConfig.LogHome, 0755)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	tgConf.setLogPath(tserverConfig.LogHome)
+	err = tgConf.setMaxMinutes(tserverConfig.MaxMinutes)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	newLogger := Logger{logname: "test"}
+	tgLoggers["test"] = &newLogger
+
+	var wg sync.WaitGroup
+	for i := 0; i != 200000; i++ {
+		wg.Add(1)
+		go func() {
+			err = twlog(tgLoggers, "test", "{\"test\": 1}")
+			if err != nil {
+				t.Error("Didn't expect error ", err)
+			}
+			wg.Add(-1)
+		}()
+	}
+	wg.Wait()
 }
